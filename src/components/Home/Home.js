c8543b12ccfddba0d2e42e90316ab850707af014
killerkc12 <kiranchavankc12@gmail.com>
2022-09-17 21:39:03 +0530

    TASK-5-Fetch-Board-TaskList

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,54 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../App'
 import { useNavigate  } from 'react-router-dom';
 import './Home.css';
 import TaskList from '../TaskList/TaskList';
+import { collection, doc, onSnapshot, query, where } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
+import Navbar from '../Navbar/Navbar';
 
 const Home = () => {
     const history = useNavigate ();
-    const {state,dispatch} = useContext(UserContext)
+    const {state,dispatch} = useContext(UserContext);
+    const [taskList, setTaskList] = useState([]);
+    const [isTaskList, setIsTaskList] = useState(false);
+
+    const GetTaskList = async () => {
+      console.log(state.default_board);
+      const q = query(collection(db, 'TaskList'), where('board_id', '==', state.default_board));
+      onSnapshot(q, (querySnapshot) => {
+        const list = [];
+        querySnapshot.docs.map((doc) => (
+          list.push({ id: doc.id, ...doc.data() }
+          )));
+        setTaskList(list);
+      })
+      setIsTaskList(true);
+    }
+
+    useEffect(() => {
+      console.log('state: ', state);
+      console.log('taskList: ', taskList);
+      if (state !== null && !isTaskList) GetTaskList();
+    }, [state])
 
   return (
-    <div className='home__container'>
-      {/* TODO: Board name */}
-      <h3>Main Board</h3>
+    <div className='main__container'>
+      { state?.uid && <Navbar /> }
+      <div className='home__container'>
+        {/* TODO: Board name */}
+        <h3>Main Board</h3>
 
-      {/* List of Task list */}
-      <div className="home__tasklist">
-        <TaskList />
-        <TaskList />
+        {/* List of Task list */}
+        <div className="home__tasklist">
+          {
+            taskList?.map((item, index) => {
+              return <div key={index}>
+                <TaskList  taskList={item} />
+              </div>
+            })
+          }
+        </div>
       </div>
     </div>
   )
--- src/components/TaskInput/TaskInput.js
+++ src/components/TaskInput/TaskInput.js
@@ -1,10 +1,34 @@
 import React, { useState } from 'react'
 import './TaskInput.css';
 import { AiOutlinePlus, AiFillPlusCircle } from 'react-icons/ai';
+import { async } from '@firebase/util';
+import { addDoc, collection } from 'firebase/firestore';
+import { db } from '../../firebase/firebase';
 
-const TaskInput = () => {
+const TaskInput = ({ tasklist_id }) => {
     const [isHover, setHover] = useState(false);
-    return (
+    const [taskName, setTaskName] = useState('');
+
+    const SetInputText = (e) => {
+        if (e.key === 'Enter' && e.target.value !== '') {
+            AddNewTask();
+        }
+    }
+
+    const AddNewTask = async () => {
+        console.log('add new task');
+        console.log('tasklist_id: ', tasklist_id);
+        const docRef = collection(db, 'Task');
+        const data = {
+            task_name: taskName,
+            tasklist_id: tasklist_id
+        }
+        const docRes = await addDoc(docRef, data);
+        setTaskName('');
+        console.log('docRes: ', docRes);
+    }
+
+     return (
       <div className="task__input_container" onMouseOut={() => setHover(false)} onMouseMove={() => setHover(true)}>
           <div className="task__button__container">
                 {   
@@ -15,7 +39,8 @@ const TaskInput = () => {
                 }
           </div>
           <div className="task__inputbox__container">
-              <input type="text" className='task__input' placeholder='Title' />
+              <input type="text" className='task__input' placeholder='Title' onKeyUp={(event) => SetInputText(event)}
+                onChange={(e) => setTaskName(e.target.value)} value={taskName} />
           </div>
       </div>
   )
