a157e8828beacc9c30526063b1ee02b9b8f203d7
Bhavik Agarwal <73033511+Bhavik-ag@users.noreply.github.com>
2022-10-03 12:50:19 +0530

    TASK-9 Remove Routing from the project (#15)
    Merging the changes

--- src/App.js
+++ src/App.js
@@ -1,29 +1,16 @@
 import './App.css';
 import { createContext, useEffect, useReducer } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { initialState, reducer } from './reducers/userReducer';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 
 export const UserContext = createContext();
 
-const Rounting = () => {
-    const user = JSON.parse(localStorage.getItem('user'));
-
-  
-
-    return(
-        <Routes>
-            <Route path="/" element={user ? <Home /> : <Login></Login>} ></Route>
-            {/* <Route path="/" element={<Home />} ></Route> */}
-            {/* <Route path="/login" element={<Login />} ></Route> */}
-        </Routes>
-    );
-};
-
 function App() {
     const [state, dispatch] = useReducer(reducer, initialState);
 
+    const user = JSON.parse(localStorage.getItem('user'));
+    
     useEffect(() => {
         const user = JSON.parse(localStorage.getItem('user'));
         if (user && state === null) {
@@ -33,9 +20,7 @@ function App() {
 
     return (
         <UserContext.Provider value={{state, dispatch}}>
-            <BrowserRouter>
-                <Rounting />
-            </BrowserRouter>
+            {user ? <Home /> : <Login></Login>}
         </UserContext.Provider>
     );
 }
