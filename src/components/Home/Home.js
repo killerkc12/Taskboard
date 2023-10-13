a0204cb071e42790cd690a17c3b8906ab646d4b4
Tipchan Songtheng <tipchans777@gmail.com>
2023-10-13 23:51:49 +0530

    add responsive design

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,100 +1,108 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../App';
-import './Home.css';
-import TaskList from '../TaskList/TaskList';
-import { collection, deleteDoc, doc, onSnapshot, query, where } from 'firebase/firestore';
-import { db } from '../../firebase/firebase';
-import Navbar from '../Navbar/Navbar';
-import TaskListInput from '../TaskListInput/TaskListInput';
-import Loader from '../Loader/Loader';
-import Popup from 'reactjs-popup';
-import { BsThreeDotsVertical } from 'react-icons/bs';
-import { MdDelete } from 'react-icons/md';
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../../App";
+import "./Home.css";
+import TaskList from "../TaskList/TaskList";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { db } from "../../firebase/firebase";
+import Navbar from "../Navbar/Navbar";
+import TaskListInput from "../TaskListInput/TaskListInput";
+import Loader from "../Loader/Loader";
+import Popup from "reactjs-popup";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { MdDelete } from "react-icons/md";
 
 const Home = () => {
-    const {state} = useContext(UserContext);
-    const [taskList, setTaskList] = useState([]);
-    const [isTaskList, setIsTaskList] = useState(false);
-    const [isTaskListLoading, setTaskListLoading] = useState(true);
+  const { state } = useContext(UserContext);
+  const [taskList, setTaskList] = useState([]);
+  const [isTaskList, setIsTaskList] = useState(false);
+  const [isTaskListLoading, setTaskListLoading] = useState(true);
 
-    const GetTaskList = async () => {
-        const q = query(collection(db, 'TaskList'), where('board_id', '==', state.default_board));
-        onSnapshot(q, (querySnapshot) => {
-            const list = [];
-            querySnapshot.docs.map((doc) => (
-                list.push({ id: doc.id, ...doc.data() }
-                )));
-            list.sort((a, b) => a.createdOn > b.createdOn ? 1 : -1);
-            setTaskList(list);
-            setTaskListLoading(false);
-        });
-        setIsTaskList(true);
-    };
-
-    useEffect(() => {
-        if (state !== null && !isTaskList) GetTaskList();
-    }, [state]);
-
-    return (
-        <div className='main__container'>
-            { state?.uid && <Navbar /> }
-            <div className='home__container'>
-                <div className='board_container'>
-                    <h3>Main Board</h3>
-                    <Popup
-                        trigger={
-                            <div className="task__options">
-                                <BsThreeDotsVertical />
-                            </div>
-                        }
-                        position="left top"
-                        on="click"
-                        closeOnDocumentClick
-                        mouseLeaveDelay={300}
-                        mouseEnterDelay={0}
-                        contentStyle={{ padding: '0px', border: 'none' }}
-                        arrowStyle={{ color: '#3A3C45' }}
-                        arrow={true}
-                    >
-                        <MenuOptions id = {state?.default_board} />
-                    </Popup>
-                </div>
-
-                {/* List of Task list */}
-                <div className="home__tasklist">
-                    {
-                        isTaskListLoading ? <Loader /> : <>
-                            {
-                                taskList?.map((item, index) => {
-                                    return <div key={index}>
-                                        <TaskList taskList={item} />
-                                    </div>;
-                                })
-                            }
-                            <TaskListInput board_id={state?.default_board} />
-                        </>
-                    }
-                </div>
-            </div>
-        </div>
+  const GetTaskList = async () => {
+    const q = query(
+      collection(db, "TaskList"),
+      where("board_id", "==", state.default_board)
     );
+    onSnapshot(q, (querySnapshot) => {
+      const list = [];
+      querySnapshot.docs.map((doc) => list.push({ id: doc.id, ...doc.data() }));
+      list.sort((a, b) => (a.createdOn > b.createdOn ? 1 : -1));
+      setTaskList(list);
+      setTaskListLoading(false);
+    });
+    setIsTaskList(true);
+  };
+
+  useEffect(() => {
+    if (state !== null && !isTaskList) GetTaskList();
+  }, [state]);
+
+  return (
+    <div className="main__container">
+      {state?.uid && <Navbar />}
+      <div className="home__container">
+        <div className="board_container">
+          <h3>Main Board</h3>
+          <Popup
+            trigger={
+              <div className="task__options">
+                <BsThreeDotsVertical />
+              </div>
+            }
+            position="left top"
+            on="click"
+            closeOnDocumentClick
+            mouseLeaveDelay={300}
+            mouseEnterDelay={0}
+            contentStyle={{ padding: 0, border: "none" }}
+            arrowStyle={{ color: "#3A3C45" }}
+            arrow={true}
+          >
+            <MenuOptions id={state?.default_board} />
+          </Popup>
+        </div>
+
+        {/* List of Task list */}
+        <div className="home__tasklist">
+          {isTaskListLoading ? (
+            <Loader />
+          ) : (
+            <>
+              {taskList?.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <TaskList taskList={item} />
+                  </div>
+                );
+              })}
+              <TaskListInput board_id={state?.default_board} />
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 // eslint-disable-next-line react/prop-types
-const MenuOptions = ({id}) => {
-    const DeleteTask = async (id) => {
-        await deleteDoc(doc(db, 'Board', id));
-    };
-    return (
-        <div className="menu">
-            <div className="menu-item"
-                onClick={() => DeleteTask(id)}
-            > 
-                <MdDelete className='menu-icon' />
-                Delete 
-            </div>
-        </div>
-    );
+const MenuOptions = ({ id }) => {
+  const DeleteTask = async (id) => {
+    await deleteDoc(doc(db, "Board", id));
+  };
+  return (
+    <div className="menu">
+      <div className="menu-item" onClick={() => DeleteTask(id)}>
+        <MdDelete className="menu-icon" />
+        Delete
+      </div>
+    </div>
+  );
 };
 
 export default Home;
--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,105 +1,102 @@
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import './Task.css';
-import { BsCircle } from 'react-icons/bs';
-import { MdDone } from 'react-icons/md';
-import { BsThreeDotsVertical } from 'react-icons/bs';
-import Popup from 'reactjs-popup';
-import { MdDelete } from 'react-icons/md';
-import { deleteDoc, doc, updateDoc } from 'firebase/firestore';
-import { db } from '../../firebase/firebase';
+import React, { useState } from "react";
+import "./Task.css";
+import { BsCircle } from "react-icons/bs";
+import { MdDone } from "react-icons/md";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import Popup from "reactjs-popup";
+import { MdDelete } from "react-icons/md";
+import { deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { db } from "../../firebase/firebase";
 
 const DeleteTask = async (id) => {
-    await deleteDoc(doc(db, 'Task', id));
+  await deleteDoc(doc(db, "Task", id));
 };
 
 const Task = (props) => {
-    const [isHover, setHover] = useState(false);
-    const [isHoverCompleted, setHoverCompleted] = useState(false);
+  const [isHover, setHover] = useState(false);
+  const [isHoverCompleted, setHoverCompleted] = useState(false);
 
-    const UpdateTaskStatus = async () => {
-        const docRef = doc(db, 'Task', props.task.id);
-        await updateDoc(docRef, {
-            isCompleted: props.isCompleted ? false : true
-        });
-    };
+  const UpdateTaskStatus = async () => {
+    const docRef = doc(db, "Task", props.task.id);
+    await updateDoc(docRef, {
+      isCompleted: props.isCompleted ? false : true,
+    });
+  };
 
-    
-    return (
-        <div className='task__container' 
-            onMouseMove={() => setHoverCompleted(true)}
-            onMouseOut={() => setHoverCompleted(false)}
-        >
-            <div className='task__start'>
-                {
-                    props.task.isCompleted ? 
-                    (
-                        <div className='task__start'>
-                            <div className="task__icon" onClick={UpdateTaskStatus}>
-                                <MdDone className='task__icon__done' />
-                            </div>
-                            <div className="task__name__completed">
-                                { props.task.task_name }
-                            </div>
-                        </div>
-                        
-                    ) : (
-                        <div className='task__start'>
-                            <div className="task__icon" onClick={UpdateTaskStatus}>
-                                { 
-                                    !isHover && <BsCircle onMouseMove={() => setHover(true)}  className='task__icon__circle' />
-                                }
-                                { 
-                                    isHover && <MdDone onMouseOut={() => setHover(false)}  className='task__icon__done' />
-                                }
-                            </div>
-                            <div className="task__name">
-                                { props.task.task_name }
-                            </div>
-                        </div>
-                    )
-                }
-                
+  return (
+    <div
+      className="task__container"
+      onMouseMove={() => setHoverCompleted(true)}
+      onMouseOut={() => setHoverCompleted(false)}
+    >
+      <div className="task__start">
+        {props.task.isCompleted ? (
+          <div className="task__start">
+            <div className="task__icon" onClick={UpdateTaskStatus}>
+              <MdDone className="task__icon__done" />
             </div>
-            {
-                props.task.isCompleted ? 
-                (
-                    isHoverCompleted && <MdDelete className='menu-icon' onClick={() => DeleteTask(props.task.id)}/>
-                ) : (
-                    <Popup
-                        trigger={
-                            <div className="task__options">
-                                <BsThreeDotsVertical />
-                            </div> 
-                        }
-                        position="right top"
-                        on="click"
-                        closeOnDocumentClick
-                        mouseLeaveDelay={300}
-                        mouseEnterDelay={0}
-                        contentStyle={{ padding: '0px', border: 'none' }}
-                        arrowStyle={{ color: '#3A3C45'}}
-                        arrow={true}
-                    >
-                        <MenuOptions id = {props.task.id} />
-                    </Popup>
-                )
-            }
-        </div>
-    );
+            <div className="task__name__completed">{props.task.task_name}</div>
+          </div>
+        ) : (
+          <div className="task__start">
+            <div className="task__icon" onClick={UpdateTaskStatus}>
+              {!isHover && (
+                <BsCircle
+                  onMouseMove={() => setHover(true)}
+                  className="task__icon__circle"
+                />
+              )}
+              {isHover && (
+                <MdDone
+                  onMouseOut={() => setHover(false)}
+                  className="task__icon__done"
+                />
+              )}
+            </div>
+            <div className="task__name">{props.task.task_name}</div>
+          </div>
+        )}
+      </div>
+      {props.task.isCompleted ? (
+        isHoverCompleted && (
+          <MdDelete
+            className="menu-icon"
+            onClick={() => DeleteTask(props.task.id)}
+          />
+        )
+      ) : (
+        <Popup
+          trigger={
+            <div className="task__options">
+              <BsThreeDotsVertical />
+            </div>
+          }
+          position="right top"
+          on="click"
+          closeOnDocumentClick
+          mouseLeaveDelay={300}
+          mouseEnterDelay={0}
+          contentStyle={{ padding: 0, border: "none" }}
+          arrowStyle={{ color: "#3A3C45" }}
+          arrow={true}
+        >
+          <MenuOptions id={props.task.id} />
+        </Popup>
+      )}
+    </div>
+  );
 };
 
 const MenuOptions = ({ id }) => {
-    return (
-        <div className="menu">
-            <div className="menu-item"
-                onClick={() => DeleteTask(id)}
-            > 
-                <MdDelete className='menu-icon' />
-                Delete 
-            </div>
-        </div>
-    );
+  return (
+    <div className="menu">
+      <div className="menu-item" onClick={() => DeleteTask(id)}>
+        <MdDelete className="menu-icon" />
+        Delete
+      </div>
+    </div>
+  );
 };
 
 export default Task;
